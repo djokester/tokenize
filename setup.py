877c879de396d3c9bbb79d8ae88887841d75ae00
Samriddhi Sinha <samriddhidjokestersinha@gmail.com>
2017-03-26 14:20:49 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Tokenizer',
+      version='0.1',
+      description='Simple Tokenizer for your Strings',
+      url='http://github.com/djokester/tokenizer',
+      author='Samriddhi Sinha',
+      author_email='samriddhidjokestersinha@gmail.com',
+      license= None)
